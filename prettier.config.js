b4e2c7484ea4e1f8b7a5f27d9c99ae3a87fff7c2
Nadav Gover <nadavgover@gmail.com>
2024-07-02 15:04:46 +0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  useTabs: false,
+  tabWidth: 2
+};
